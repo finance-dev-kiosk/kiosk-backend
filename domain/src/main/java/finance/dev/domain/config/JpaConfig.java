af21526cf0994fccb6c283dfa6a12d2c543d1112
pocj8ur4in <pocj8ur4in@gmail.com>
2024-04-29 15:25:05 +0900

    init/domain: spring-boot-starter-jpa 적용

---
+++ domain/src/main/java/finance/dev/domain/config/JpaConfig.java
@@ -0,0 +1,12 @@
+package finance.dev.domain.config;
+
+import finance.dev.common.annotation.TypeInfo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@TypeInfo(name = "JpaConfig", description = "JPA 설정 클래스")
+@Configuration
+@EnableJpaRepositories(basePackages = "finance.dev.domain.**")
+@EnableJpaAuditing
+public class JpaConfig {}
