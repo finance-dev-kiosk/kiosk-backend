3299cbc61258cbaf3c8d060b7598908bcb127f89
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-05 21:01:18 +0900

    feat/user: 사용자 닉네임 중복확인 구현
    #6

--- api/src/main/java/finance/dev/api/usecase/user/UserChkUserNameUseCase.java
+++ api/src/main/java/finance/dev/api/usecase/user/UserChkUserNameUseCase.java
@@ -16,7 +16,22 @@ public class UserChkUserNameUseCase {
     @MethodInfo(name = "execute", description = "사용자 닉네임 중복 확인을 처리합니다.")
     public ResponseEntity<Void> execute(UserChkUserNamePostRequest userChkUserNamePostRequest)
             throws Exception {
-        return null;
+        try {
+            // 유효성 검사. 아이디에 입력되지 않은 값이 있는 경우
+            if (userChkUserNamePostRequest.getName() == null) {
+                throw new BadRequestException("닉네임을 입력해주세요.");
+            }
+            // 아이디 중복 확인
+            if (userService.isExistName(userChkUserNamePostRequest.getName())) {
+                throw new BadRequestException("이미 사용 중인 닉네임입니다.");
+            }
+            // 반환
+            return ResponseEntity.ok().build();
+        } catch (BadRequestException e) {
+            throw new BadRequestException(e.getMessage());
+        } catch (Exception e) {
+            throw new Exception("닉네임 중복 확인 중 오류가 발생했습니다.");
+        }
     }
 
     public UserChkUserNameUseCase(UserService userService) {
