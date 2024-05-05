b85d0d54d6acd7c44cc522d87b252c7060d1e171
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-05 18:23:31 +0900

    init/domain: jwt 핸들러 수정
    1. 파싱된 반환값이 userId가 되도록 수정
    2. 검증 로직 추가 (IllegalStateException 발생)

--- domain/src/main/java/finance/dev/domain/handler/JwtHandler.java
+++ domain/src/main/java/finance/dev/domain/handler/JwtHandler.java
@@ -35,31 +35,51 @@ public class JwtHandler {
     }
 
     @MethodInfo(name = "parseAccessToken", description = "JWT AccessToken을 파싱합니다.")
-    public Jws<Claims> parseAccessToken(String token) {
-        return parseToken(token);
+    public String parseAccessToken(String token) {
+        try {
+            if (parseToken(token).getBody().getExpiration().before(new Date())) {
+                throw new IllegalStateException("토큰이 만료되었습니다.");
+            }
+
+            return parseToken(token).getBody().getSubject();
+        } catch (ExpiredJwtException e) {
+            throw new IllegalStateException(e.getMessage());
+        }catch (Exception e) {
+            throw new IllegalStateException("토큰이 올바르지 않습니다.");
+        }
     }
 
     @MethodInfo(name = "parseRefreshToken", description = "JWT RefreshToken을 파싱합니다.")
-    public Jws<Claims> parseRefreshToken(String token) {
-        return parseToken(token);
+    public String parseRefreshToken(String token) {
+        return validToken(token);
     }
 
     @MethodInfo(name = "buildToken", description = "JWT Token을 빌드합니다.")
     private String buildAccessToken(String id, Date issuedAt, Date tokenExpiresIn, String type) {
-        SecretKey encodedKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-
         return Jwts.builder()
                 .setSubject(id)
                 .setIssuedAt(issuedAt)
                 .setExpiration(tokenExpiresIn)
                 .claim("type", type)
-                .signWith(encodedKey)
+                .signWith(getSecretKey())
                 .compact();
     }
 
     @MethodInfo(name = "parseToken", description = "JWT Token을 파싱합니다.")
-    public Jws<Claims> parseToken(String token) {
+    private Jws<Claims> parseToken(String token) {
         return Jwts.parserBuilder().setSigningKey(getSecretKey()).build().parseClaimsJws(token);
+    }
+
+    @MethodInfo(name = "validToken", description = "JWT Token을 유효성 검사합니다.")
+    private String validToken(String token) {
+        if (parseToken(token).getBody().get("type") != "REFRESH") {
+            throw new IllegalStateException("토큰이 올바르지 않습니다.");
+        }
+        if (parseToken(token).getBody().getExpiration().before(new Date())) {
+            throw new IllegalStateException("토큰이 만료되었습니다.");
+        }
+
+        return parseToken(token).getBody().getSubject();
     }
 
     @MethodInfo(name = "getSecretKey", description = "JWT Secret Key를 생성합니다.")
