c25dfc05a9dd9cea1fd5fe3dc8d88a04e965af9c
ahyoungss <sallyajoa.com@gmail.com>
2024-05-07 14:16:15 +0900

    feat/admin: 관리자 회원 목록 조회

--- domain/src/main/java/finance/dev/domain/service/AdminService.java
+++ domain/src/main/java/finance/dev/domain/service/AdminService.java
@@ -2,15 +2,9 @@ package finance.dev.domain.service;
 
 import finance.dev.common.annotation.TypeInfo;
 import finance.dev.domain.entity.AdminEntity;
-import finance.dev.domain.entity.UserEntity;
 import finance.dev.domain.repository.jpa.AdminRepository;
 import finance.dev.domain.repository.jpa.UserRepository;
-import finance.dev.domain.type.UserSearchType;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 
 @TypeInfo(name = "AdminService", description = "관리자 서비스 클래스")
@@ -24,11 +18,7 @@ public class AdminService {
 
     public AdminEntity findByUserId(String adminId){
         return adminRepository.findById(adminId);
-    };
-
-//    public boolean isExistsId(String adminId) {
-//        return adminRepository.existsById(adminId);
-//    }
+    }
 
     public boolean isExistId(String userId) {
         return adminRepository.findById(userId) != null;
