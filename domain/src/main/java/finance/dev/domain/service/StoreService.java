e0e0ea2264c97c9a77cdd8d55b43b7bd128ff25b
ahyoungss <sallyajoa.com@gmail.com>
2024-05-08 12:45:41 +0900

    feat/관리자: 관리자 가게 목록 조회 수정

--- domain/src/main/java/finance/dev/domain/service/StoreService.java
+++ domain/src/main/java/finance/dev/domain/service/StoreService.java
@@ -59,29 +59,29 @@ public class StoreService {
             String searchValue,
             int searchPageNum,
             int searchPageSize,
-            StoreSearchSort userSearchSort,
-            StoreSearchType userSearchType) {
+            StoreSearchSort storeSearchSort,
+            StoreSearchType storeSearchType) {
 
         StringBuilder queryString = new StringBuilder("SELECT s FROM StoreEntity s");
 
-        if (userSearchType == StoreSearchType.NAME) {
+        if (storeSearchType == StoreSearchType.NAME) {
             queryString.append(" WHERE s.name LIKE '%").append(searchValue).append("%'");
-        } else if (userSearchType == StoreSearchType.CATEGORY) {
+        } else if (storeSearchType == StoreSearchType.CATEGORY) {
             queryString.append(" WHERE s.category LIKE '%").append(searchValue).append("%'");
-        } else if (userSearchType == StoreSearchType.ENTIRE) {
+        } else if (storeSearchType == StoreSearchType.ENTIRE) {
             queryString.append(" WHERE s.name LIKE '%").append(searchValue).append("%'");
             queryString.append(" OR s.category LIKE '%").append(searchValue).append("%'");
         } else {
             queryString.append(" WHERE s.name LIKE '%").append(searchValue).append("%'");
         }
 
-        if (userSearchSort == StoreSearchSort.IDX_ASC) {
+        if (storeSearchSort == StoreSearchSort.IDX_ASC) {
             queryString.append(" ORDER BY s.idx ASC");
-        } else if (userSearchSort == StoreSearchSort.IDX_DESC) {
+        } else if (storeSearchSort == StoreSearchSort.IDX_DESC) {
             queryString.append(" ORDER BY s.idx DESC");
-        } else if (userSearchSort == StoreSearchSort.NAME_ASC) {
+        } else if (storeSearchSort == StoreSearchSort.NAME_ASC) {
             queryString.append(" ORDER BY s.name ASC");
-        } else if (userSearchSort == StoreSearchSort.NAME_DESC) {
+        } else if (storeSearchSort == StoreSearchSort.NAME_DESC) {
             queryString.append(" ORDER BY s.name DESC");
         } else {
             queryString.append(" ORDER BY s.idx ASC");
